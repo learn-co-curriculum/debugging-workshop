f21ae4fc20ffed19c5014c9bb40ff45db8ee70de
Helen Liutongco <liutongcoh@gmail.com>
2019-05-13 15:09:36 -0400

    add bugs

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
+  const jokeList = document.getElementById('joke-list')
+  const newJokeLi = document.createElement('li')
+  const username = document.getElementById('name-input').value
+  let joke;
 
   function fetchJoke(){
-    return fetch('https://icanhazdadjoke.com/', {
+    fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => jokeData.joke)
+    .then(jokeData => joke = jokeData.joke)
   }
 
   form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const username = document.getElementById('name-input').value
-    const jokeList = document.getElementById('joke-list')
-    const newJokeLi = document.createElement('li')
 
     if(username === "") return;
-
-    fetchJoke().then(joke => {
-
-      newJokeLi.innerHTML = `
-      <span class="username">${username} says:</span> ${joke}
-      `
-
-      jokeList.appendChild(newJokeLi)
-    })
+    fetchJoke()
+    newJokeLi.innerHTML = `
+    <span class="username">${username} says:</span> ${joke}
+    `
+    jokeList.appendChild(newJokeLi)
   })
 })
